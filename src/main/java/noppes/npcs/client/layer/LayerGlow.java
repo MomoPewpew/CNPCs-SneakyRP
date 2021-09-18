977863b066a5d49ffcb4de2314245f3d1d4bca1f
marijntepas <marijntepas@gmail.com>
2021-09-18 14:26:25 +0200

    Update LayerGlow.java

--- src/main/java/noppes/npcs/client/layer/LayerGlow.java
+++ src/main/java/noppes/npcs/client/layer/LayerGlow.java
@@ -3,6 +3,7 @@ package noppes.npcs.client.layer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -17,14 +18,19 @@ public class LayerGlow implements LayerRenderer {
 		this.renderer = npcRenderer;
 	}
 
-	public void doRenderLayer(EntityNPCInterface npc, float limbSwing, float limbSwingAmount, float partialTicks,
+	public boolean shouldCombineTextures() {
+		return true;
+	}
+
+	@Override
+	public void doRenderLayer(EntityLivingBase npc, float limbSwing, float limbSwingAmount, float partialTicks,
 			float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-		if (!npc.display.getOverlayTexture().isEmpty()) {
-			if (npc.textureGlowLocation == null) {
-				npc.textureGlowLocation = new ResourceLocation(npc.display.getOverlayTexture());
+		if (!((EntityNPCInterface) npc).display.getOverlayTexture().isEmpty()) {
+			if (((EntityNPCInterface) npc).textureGlowLocation == null) {
+				((EntityNPCInterface) npc).textureGlowLocation = new ResourceLocation(((EntityNPCInterface) npc).display.getOverlayTexture());
 			}
 
-			this.renderer.bindTexture(npc.textureGlowLocation);
+			this.renderer.bindTexture(((EntityNPCInterface) npc).textureGlowLocation);
 			GlStateManager.enableBlend();
 			GlStateManager.blendFunc(1, 1);
 			GlStateManager.disableLighting();
@@ -35,16 +41,12 @@ public class LayerGlow implements LayerRenderer {
 			OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) i / 1.0F, (float) j / 1.0F);
 			GlStateManager.enableLighting();
 			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-			this.renderer.getMainModel().render(npc, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch,
+			this.renderer.getMainModel().render(((EntityNPCInterface) npc), limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch,
 					scale);
-			this.renderer.setLightmap(npc);
+			this.renderer.setLightmap(((EntityNPCInterface) npc));
 			GlStateManager.disableBlend();
 			GlStateManager.enableAlpha();
 			GlStateManager.depthFunc(515);
 		}
-	}
-
-	public boolean shouldCombineTextures() {
-		return true;
 	}
 }
