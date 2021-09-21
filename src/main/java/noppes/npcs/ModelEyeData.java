b447ac65def8bc7251d3d74786c793fae3ef8373
marijntepas <marijntepas@gmail.com>
2021-09-21 16:06:58 +0200

    Set default eyes to none

--- src/main/java/noppes/npcs/ModelEyeData.java
+++ src/main/java/noppes/npcs/ModelEyeData.java
@@ -19,6 +19,7 @@ public class ModelEyeData extends ModelPartData {
 		this.color = (new Integer[] { 8368696, 16247203, 10526975, 10987431, 10791096, 4210943, 14188339, 11685080,
 				6724056, 15066419, 8375321, 15892389, 10066329, 5013401, 8339378, 3361970, 6704179, 6717235, 10040115,
 				16445005, 6085589, 4882687, 55610 })[this.r.nextInt(23)];
+		this.type = -1;
 	}
 
 	public NBTTagCompound writeToNBT() {
